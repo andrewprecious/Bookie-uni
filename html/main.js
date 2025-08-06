ef641088432848d3b4fd9dc35293f82c3b9cf0d7
andrewprecious <lilianprecious753@gmail.com>
2025-08-06 13:04:29 +0100

    initial-commit

--- html/main.js
+++ html/main.js
@@ -3,6 +3,11 @@ const closeDropdown = document.getElementById("closeStudyDropdown");
 const hamCloseBtn = document.getElementById("hamCloseBtn");
 const loginBtn = document.getElementById("loginBtn");
 const dropDownMenu = document.querySelector(".dropDownMenu");
+const loginMenu = document.getElementById("loginMenu");
+// alternate header starts
+const loginBtnTwo = document.getElementById("loginBtnTwo");
+const dropDownMenuTwo = document.querySelector(".dropDownMenuTwo");
+const loginMenuTwo = document.getElementById("loginMenuTwo");
 
 const originalHeader = document.getElementById("originalHeader");
 const alternateHeader = document.getElementById("alternateHeader");
@@ -10,12 +15,22 @@ const dropdownOverlay = document.getElementById("dropDownStudy");
 const navBottomOriginal = document.getElementById("navBottomOriginal");
 const logoContainerOriginal = document.getElementById("logoContainerOriginal");
 
+// loginBtn.addEventListener("click", () => {
+//   if (dropDownMenu.style.display === "flex") {
+//     dropDownMenu.style.display = "none";
+//   } else {
+//     dropDownMenu.style.display = "flex";
+//   }
+// });
+
 loginBtn.addEventListener("click", () => {
-  if (dropDownMenu.style.display === "flex") {
-    dropDownMenu.style.display = "none";
-  } else {
-    dropDownMenu.style.display = "flex";
-  }
+  loginMenu.style.display =
+    loginMenu.style.display === "flex" ? "none" : "flex";
+});
+
+loginBtnTwo.addEventListener("click", () => {
+  loginMenuTwo.style.display =
+    loginMenuTwo.style.display === "flex" ? "none" : "flex";
 });
 
 openDropdown.addEventListener("click", () => {
